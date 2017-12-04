5918bd36d8f7b35cfc1ffbcfc50e5aa9b91709b2
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-04 19:04:14 +0000

    Mon Dec 04 19:04:14 GMT 2017

--- src/BeFaster.App.Tests/Solutions/CheckoutSolutionTests.cs
+++ src/BeFaster.App.Tests/Solutions/CheckoutSolutionTests.cs
@@ -1,6 +1,15 @@
-﻿namespace BeFaster.App.Tests.Solutions
+﻿using BeFaster.App.Solutions;
+using NUnit.Framework;
+
+namespace BeFaster.App.Tests.Solutions
 {
+    [TestFixture]
     public class CheckoutSolutionTests
     {
+        [TestCase("AABBCCDDAAB", 325)]
+        public void Checkout(string skus, int expectedTotal)
+        {
+            Assert.That(CheckoutSolution.Checkout(skus), Is.EqualTo(325));
+        }
     }
 }
