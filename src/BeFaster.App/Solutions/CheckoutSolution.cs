971e89640b4c58cdd0f2dd00ab0bec5149980a64
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-04 19:25:14 +0000

    Mon Dec 04 19:25:14 GMT 2017

--- src/BeFaster.App/Solutions/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CheckoutSolution.cs
@@ -1,32 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
-using BeFaster.Runner.Exceptions;
 
 namespace BeFaster.App.Solutions
 {
     public static class CheckoutSolution
     {
-        private static readonly PriceList PriceList = new PriceList();
+        private static readonly CheckoutEngine _checkoutEngine = new CheckoutEngine();
 
         public static int Checkout(string skus)
         {
             foreach (char sku in skus)
             {
-                if (!PriceList.Contains(sku)) return -1;
+                _checkoutEngine.Add(sku);
             }
-            throw new Exception();
+            return _checkoutEngine.CalculateTotal();
         }
 
         
     }
 
-    public class PriceList
+    public class CheckoutEngine
     {
-        private readonly IDictionary<char, Item> _items = new Dictionary<char, Item>();
+        private readonly IDictionary<char, Item> _priceList = new Dictionary<char, Item>();
+        private IList<Item> _basket = new List<Item>();
 
         public bool Contains(char sku)
         {
-            return _items.ContainsKey(sku);
+            return _priceList.ContainsKey(sku);
+        }
+
+        public void Add(char sku)
+        {
+            if (!_priceList.ContainsKey(sku))
+            {
+                _basket.Add(null);
+                return;
+            }
+            _basket.Add(_priceList[sku]);
+        }
+
+        public int CalculateTotal()
+        {
+            if (_basket.Contains(null)) return -1;
+            return 100;
         }
     }
 
