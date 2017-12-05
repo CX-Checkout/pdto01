66b0c4cd9a2c15dba20ced3efd9158579c2a91ad
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-05 13:48:05 +0000

    Tue Dec 05 13:48:05 GMT 2017

---
+++ src/BeFaster.App/Checkout/GroupDiscount.cs
@@ -0,0 +1,10 @@
+namespace BeFaster.App.Checkout
+{
+    public class GroupDiscount : IDiscount
+    {
+        public GroupDiscount(string stxyz, int i, int i1)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/BeFaster.App/Checkout/IDiscount.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace BeFaster.App.Checkout
+{
+    public interface IDiscount
+    {
+        bool CanApplyTo(IList<Item> itemsLeft);
+        int ApplyDiscount(ref IList<Item> itemsLeft);
+        int DiscountValue { get; }
+    }
+}
