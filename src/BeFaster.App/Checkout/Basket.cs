17c39180b1d0fc9b6176294a872b811bd5d4bbeb
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-05 10:33:15 +0000

    Tue Dec 05 10:33:15 GMT 2017

--- src/BeFaster.App/Checkout/Basket.cs
+++ src/BeFaster.App/Checkout/Basket.cs
@@ -5,47 +5,26 @@ namespace BeFaster.App.Checkout
 {
     public class Basket
     {
-        private readonly IList<Item> _basket = new List<Item>();
+        private readonly IList<Item> _items = new List<Item>();
 
         public void Add(Item item)
         {
-            _basket.Add(item);
+            _items.Add(item);
         }
 
         public int CalculateTotal(IList<Discount> configuredDiscounts)
         {
-            if (_basket.Contains(null)) return -1;
-            var total = _basket.Select(x => x.Price).Sum();
-            return total - CalculateDiscount(configuredDiscounts);
+            if (_items.Contains(null)) return -1;
+            var total = _items.Select(x => x.Price).Sum();
+            var discount = configuredDiscounts != null ? new DiscountEngine(configuredDiscounts).CalculateDiscount(_items) : 0;
+            return total - discount;
         }
 
         private int CalculateDiscount(IList<Discount> configuredDiscounts)
         {
-            int totalDiscount = 0;
-            foreach (Discount discount in configuredDiscounts)
-            {
-                totalDiscount += CalculateItemDiscount(discount);
-            }
-            return totalDiscount;
+            
         }
 
-        private int CalculateItemDiscount(Discount discount)
-        {
-            var items = _basket.Where(item => item.Sku == discount.Sku);
-            if (!items.Any()) return 0;
-
-            int discountAmount = 0;
-            if (items.Count() >= discount.Max())
-            {
-                var applicableTimes = _basket.Count(x => x.Sku.Equals(discount.Sku)) / discount.Max();
-                discountAmount += applicableTimes * discount.MaxValue();
-            }
-            if (discountAmount >= 0 && discount.Min()!= discount.Max())
-            {
-                var applicableTimes = (_basket.Count(x => x.Sku.Equals(discount.Sku)) % discount.Max()) / discount.Min();
-                discountAmount += applicableTimes * discount.MinValue();
-            }
-            return discountAmount;
-        }
+       
     }
 }
---
+++ src/BeFaster.App/Checkout/DiscountEngine.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BeFaster.App.Checkout
+{
+    public class DiscountEngine
+    {
+        private readonly IList<Discount> _configuredDiscounts;
+        private IList<Item> _items;
+
+        public DiscountEngine(IList<Discount> configuredDiscounts)
+        {
+            _configuredDiscounts = configuredDiscounts;
+        }
+
+        public int CalculateDiscount(IList<Item> items)
+        {
+            _items = items;
+            int totalDiscount = 0;
+            foreach (Discount discount in _configuredDiscounts)
+            {
+                totalDiscount += CalculateItemDiscount(discount);
+            }
+            return totalDiscount;
+        }
+
+        private int CalculateItemDiscount(Discount discount)
+        {
+            var items = _items.Where(item => item.Sku == discount.Sku);
+            if (!items.Any()) return 0;
+
+            int discountAmount = 0;
+            if (items.Count() >= discount.Max())
+            {
+                var applicableTimes = _items.Count(x => x.Sku.Equals(discount.Sku)) / discount.Max();
+                discountAmount += applicableTimes * discount.MaxValue();
+            }
+            if (discountAmount >= 0 && discount.Min() != discount.Max())
+            {
+                var applicableTimes = (_items.Count(x => x.Sku.Equals(discount.Sku)) % discount.Max()) / discount.Min();
+                discountAmount += applicableTimes * discount.MinValue();
+            }
+            return discountAmount;
+        }
+    }
+}
