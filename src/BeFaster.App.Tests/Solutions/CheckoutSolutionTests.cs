256dfc6eecdb746ef817eb973fffaa0c3b375990
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-12-05 13:21:23 +0000

    Tue Dec 05 13:21:23 GMT 2017

--- src/BeFaster.App.Tests/Solutions/CheckoutSolutionTests.cs
+++ src/BeFaster.App.Tests/Solutions/CheckoutSolutionTests.cs
@@ -6,12 +6,16 @@ namespace BeFaster.App.Tests.Solutions
     [TestFixture]
     public class CheckoutSolutionTests
     {
+        [TestCase("HHHHHGHHHHHGHHHHH", 165)]
         [TestCase("AABBCCDDAABEEFFF", 395)]
         [TestCase("AABBCCDDAABEE", 375)]
         [TestCase("ABCDEE", 165)]
+        [TestCase("PPPPP", 200)]
+        [TestCase("PPPPPBB", 245)]
         [TestCase("AABBCCDDAAB", 325)]
         [TestCase("AAAAA", 200)]
         [TestCase("BB", 45)]
+        [TestCase("KK", 150)]
         [TestCase("FFF", 20)]
         [TestCase("AAAAAAAA", 330)]
         [TestCase("AAAAAAAAAAAAA", 530)]
--- src/BeFaster.App/Checkout/CheckoutEngine.cs
+++ src/BeFaster.App/Checkout/CheckoutEngine.cs
@@ -13,6 +13,26 @@ namespace BeFaster.App.Checkout
             {'D', new Item('D', 15)},
             {'E', new Item('E', 40)},
             {'F', new Item('F', 10)},
+            {'G', new Item('G', 20)},
+            {'H', new Item('H', 10)},
+            {'I', new Item('I', 35)},
+            {'J', new Item('J', 60)},
+            {'K', new Item('K', 80)},
+            {'L', new Item('L', 90)},
+            {'M', new Item('M', 15)},
+            {'N', new Item('N', 40)},
+            {'O', new Item('O', 10)},
+            {'P', new Item('P', 50)},
+            {'Q', new Item('Q', 30)},
+            {'R', new Item('R', 50)},
+            {'S', new Item('S', 30)},
+            {'T', new Item('T', 20)},
+            {'U', new Item('U', 40)},
+            {'V', new Item('V', 50)},
+            {'W', new Item('W', 20)},
+            {'X', new Item('X', 90)},
+            {'Y', new Item('Y', 10)},
+            {'Z', new Item('Z', 50)}
         };
 
         private readonly IList<IDiscount> configuredDiscounts = new List<IDiscount>
@@ -22,6 +42,9 @@ namespace BeFaster.App.Checkout
             new Discount('B', 2, 15),
             new Discount('F', 3, 10),
             new FreeItemDiscount('E', 2, new Item('B', 30)),
+            new Discount('H', 5, 5),
+            new Discount('H', 10, 20),
+            new Discount('K', 2, 10),
         };
 
         private readonly Basket _basket = new Basket();
